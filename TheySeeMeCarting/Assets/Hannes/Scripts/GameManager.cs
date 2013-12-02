1ca79aac1af0c8c0e23563cebad70971f49d1d1d
Hannes Lidbeck <hannes.lidbeck@futuregames.se>
2013-12-02 22:16:09 +0100

    #9

--- TheySeeMeCarting/Assets/Hannes/Scripts/GameManager.cs
+++ TheySeeMeCarting/Assets/Hannes/Scripts/GameManager.cs
@@ -8,7 +8,7 @@ public class GameManager : MonoBehaviour {
 	public List<Cart> carts = new List<Cart>();
 	public List<GameObject> playerPrefabs = new List<GameObject>(); // This is where you put the playerPrefabs to create lists from, probably good to make this more generic
 
-	public CartSpawner cartSpawner;
+	//public CartSpawner cartSpawner;
 
 
 	void Start ()
@@ -24,7 +24,7 @@ public class GameManager : MonoBehaviour {
 			Player player = (Instantiate(playerPrefab) as GameObject).GetComponent<Player>();
 			players.Add(player); // Adds a new player to the game
 
-			Cart cart = player.AssignCart(); // Accessing the spawning function from the Spawn initiate script
+			Cart cart = player.spawner.SpawnCart(); // Accessing the spawning function from the Spawn initiate script
 			carts.Add(cart); // Adds a new cart to the Game manager list
 		}
 	}
